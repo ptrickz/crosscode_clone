f3c731e3a0dee337757f4fe3d111f0494dd20fd6
Pattrick Pengiran <pattrickpatt@gmail.com>
2025-08-22 00:06:22 +0800

    feat: changed to using state management

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -2,24 +2,19 @@ class_name Player extends CharacterBody2D
 
 var cardinal_direction : Vector2 = Vector2.DOWN
 var direction : Vector2 = Vector2.ZERO
-var move_speed : float = 100.0
-var state : String = "idle"
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var state_machine : PlayerStateMachine = $StateMachine
 
 func _ready():
+	state_machine.Initialize(self)
 	pass
 
 func _process(delta):
 	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
-	velocity = direction * move_speed
-	
-	if SetState() == true || SetDirection() == true:
-		UpdateAnimation()
 
 	pass
 
@@ -44,14 +39,7 @@ func  SetDirection() -> bool :
 	return true
 	
 
-func SetState() -> bool :
-	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state:
-		return false
-	state = new_state
-	return true
-	
-func UpdateAnimation() -> void:
+func UpdateAnimation( state: String) -> void:
 	var animation : String =  state + "_" + AnimDirection()
 	animation_player.play(animation)
 	print(animation)
---
+++ Player/Scripts/state.gd
@@ -0,0 +1,21 @@
+class_name State extends Node
+
+static var player : Player
+
+func _ready() :
+     pass
+
+func Enter() -> void:
+    pass
+
+func Exit() -> void:
+    pass
+
+func Process(_delta : float) -> State:
+    return null
+
+func Physics(_delta : float) -> State:
+    return null
+
+func HandleInput(_delta : InputEvent) -> State:
+    return null
